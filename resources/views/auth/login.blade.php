93c7099ea21277359069632534fe372e381d5c4d
Anil <kumarglci>
2022-05-28 12:21:06 +0530

    project setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Laravel 6 - Login with Google Account Example - ItSolutionStuuf.com</div>
+                <div class="card-header">Test Project</div>
   
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
